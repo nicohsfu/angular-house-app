5edb99fa31d3f0b557e1daff20d9a0d13f756e46
Nico Hernandez <marcnicohernandez@gmail.com>
2023-11-09 17:54:07 -0800

    created new property decorated with @Input() decorator

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
@@ -13,5 +14,8 @@ import { CommonModule } from '@angular/common';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  // below, we're adding a property called housingLocation of type HousingLocation
+  // we're adding a "!" because the input is expecting a value to be passed, and there's no default value
+  // "!" notifies the compiler that the value of the new property won't be null or undefined
+  @Input() housingLocation!: HousingLocation;
 }
